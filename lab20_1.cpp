5de2b3cc3662bfcba04d549cccaca530e6e5d9eb
ratchapon prangthong <ppete2547@gamil.com>
2023-02-21 16:36:34 +0700

    lab20_1 fish

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
 
+double a = (min(((R1.x)+R1.w),((R2.x)+R2.w)) - max(R1.x,R2.x))*(min(R1.y,R2.y) - max(((R1.y)-R1.h),((R2.y)-R2.h)));
 
+if( a > 0){
+return a;
 
+}
+else {
+return 0;
+}
 }
